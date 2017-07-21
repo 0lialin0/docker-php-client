fb1ae4e65088f949b02da6ed4464e7a6b9146e9b
Li Chunlin <lichunlin@zxisl.com>
2017-07-21 09:13:59 +0800

    增加ssl 以及相关参数

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -1,6 +1,7 @@
 <?php
 namespace jarkt\docker;
 use jarkt\docker\requestHandlers\RequestHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * Class ApiClient
@@ -8,146 +9,170 @@ use jarkt\docker\requestHandlers\RequestHandler;
 class ApiClient
 {
 
-	private $host;
-	private $port;
-	private $version;
+    private $host;
+    private $port;
+    private $ssl;
+    private $version;
+    private $optionList;
 
+    /**
+     * ApiClient constructor.
+     * @param $host
+     * @param $port
+     * @param null $version
+     */
+    public function __construct($host, $port,  $optionList = [], $ssl = true, $version = null)
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->version = $version;
+        $this->ssl = $ssl;
+        $this->optionList = $optionList;
+    }
 
-	/**
-	 * ApiClient constructor.
-	 * @param $host
-	 * @param $port
-	 * @param null $version
-	 */
-	public function __construct($host, $port, $version = null)
-	{
-		$this->host = $host;
-		$this->port = $port;
-		$this->version = $version;
-	}
+    /**
+     * @param $path
+     * @param array $params
+     * @return string
+     * @throws Exception
+     */
+    private function getUrl($path, array $params = [])
+    {
+        if($path[0] !== '/') {
+            throw new \Exception('$path must begin with /');
+        }
+        $version = $this->version ? '/'.$this->version : '';
 
-	/**
-	 * @param $path
-	 * @param array $params
-	 * @return string
-	 * @throws Exception
-	 */
-	private function getUrl($path, array $params = [])
-	{
-		if($path[0] !== '/') {
-			throw new \Exception('$path must begin with /');
-		}
-		$version = $this->version ? '/'.$this->version : '';
-		$url = "http://{$this->host}:{$this->port}$version$path";
-		$query = http_build_query($params);
-		$url = strlen($query) ? "$url?$query" : $url;
-		return $url;
-	}
+        if ($this->ssl) {
+            $url = "https://{$this->host}:{$this->port}$version$path";
+        } else{
+            $url = "http://{$this->host}:{$this->port}$version$path";
+        }
 
-	/**
-	 * @param $url
-	 * @param callable|null $callback
-	 * @param RequestHandler|null $requestHandler
-	 * @return Response
-	 */
-	private function makeRequest($url, callable $callback = null, RequestHandler $requestHandler = null)
-	{
-		$response = new Response();
-		$ch = $response->getCurlHandle();
+        $query = http_build_query($params);
+        $url = strlen($query) ? "$url?$query" : $url;
+        return $url;
+    }
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 1);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($ch, $str) use($response) {
-			$response->writeData($str);
-			return strlen($str);
-		});
+    /**
+     * @param $url
+     * @param callable|null $callback
+     * @param RequestHandler|null $requestHandler
+     * @return Response
+     */
+    private function makeRequest($url, callable $callback = null, RequestHandler $requestHandler = null)
+    {
+        $response = new Response();
+        $ch = $response->getCurlHandle();
 
-		if(is_callable($callback)) {
-			call_user_func($callback, $ch);
-		}
+        curl_setopt($ch, CURLOPT_URL, $url);
 
-		if($requestHandler instanceof RequestHandler) {
-			$requestHandler->handle($ch);
-		}
+        if (count($this->optionList))
+        {
+            foreach ($this->optionList as $option => $value)
+            {
+                curl_setopt($ch, $option, $value);
+            }
+        }
 
-		$response->waitForHeader();
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($ch, $str) use($response) {
+            $response->writeData($str);
+            return strlen($str);
+        });
 
-		return $response;
-	}
+        if(is_callable($callback)) {
+            call_user_func($callback, $ch);
+        }
 
-	/**
-	 * @param $path
-	 * @param array $params
-	 * @return Response
-	 * @throws Exception
-	 */
-	public function get($path, array $params = [], RequestHandler $requestHandler = null)
-	{
-		$url = $this->getUrl($path, $params);
-		return $this->makeRequest($url, null, $requestHandler);
-	}
+        if($requestHandler instanceof RequestHandler) {
+            $requestHandler->handle($ch);
+        }
 
-	/**
-	 * @param $path
-	 * @param array $params
-	 * @return Response
-	 * @throws Exception
-	 */
-	public function head($path, array $params = [], RequestHandler $requestHandler = null)
-	{
-		$url = $this->getUrl($path, $params);
-		$callback = function($ch) {
-			curl_setopt($ch, CURLOPT_NOBODY, true);
-		};
-		return $this->makeRequest($url, $callback, $requestHandler);
-	}
+        $response->waitForHeader();
 
-	/**
-	 * @param $path
-	 * @param array $params
-	 * @param RequestHandler|null $requestHandler
-	 * @return Response
-	 * @throws \Exception
-	 */
-	public function delete($path, array $params = [], RequestHandler $requestHandler = null)
-	{
-		$url = $this->getUrl($path, $params);
-		$callback = function($ch) {
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-		};
-		return $this->makeRequest($url, $callback, $requestHandler);
-	}
+        if ($response->getStatus() == 0)
+        {
+            throw new HttpException(503, '连接超时');
+        }
 
-	/**
-	 * @param $path
-	 * @param array $params
-	 * @param RequestHandler|null $requestHandler
-	 * @return Response
-	 * @throws Exception
-	 */
-	public function post($path, array $params = [], RequestHandler $requestHandler = null)
-	{
-		$url = $this->getUrl($path, $params);
-		$callback = function($ch) {
-			curl_setopt($ch, CURLOPT_POST, 1);
-		};
-		return $this->makeRequest($url, $callback, $requestHandler);
-	}
+        return $response;
+    }
+    
+    /**
+     * @param $path
+     * @param array $params
+     * @return Response
+     * @throws Exception
+     */
+    public function get($path, array $params = [], RequestHandler $requestHandler = null)
+    {
+        $url = $this->getUrl($path, $params);
 
-	/**
-	 * @param $path
-	 * @param array $params
-	 * @param RequestHandler|null $requestHandler
-	 * @return Response
-	 * @throws Exception
-	 */
-	public function put($path, array $params = [], RequestHandler $requestHandler = null)
-	{
-		$url = $this->getUrl($path, $params);
-		$callback = function($ch) {
-			curl_setopt($ch, CURLOPT_PUT, 1);
-		};
-		return $this->makeRequest($url, $callback, $requestHandler);
-	}
+        return $this->makeRequest($url, null, $requestHandler);
+    }
+
+    /**
+     * @param $path
+     * @param array $params
+     * @return Response
+     * @throws Exception
+     */
+    public function head($path, array $params = [], RequestHandler $requestHandler = null)
+    {
+        $url = $this->getUrl($path, $params);
+        $callback = function($ch) {
+            curl_setopt($ch, CURLOPT_NOBODY, true);
+        };
+        return $this->makeRequest($url, $callback, $requestHandler);
+    }
+
+    /**
+     * @param $path
+     * @param array $params
+     * @param RequestHandler|null $requestHandler
+     * @return Response
+     * @throws \Exception
+     */
+    public function delete($path, array $params = [], RequestHandler $requestHandler = null)
+    {
+        $url = $this->getUrl($path, $params);
+        $callback = function($ch) {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        };
+        return $this->makeRequest($url, $callback, $requestHandler);
+    }
+
+    /**
+     * @param $path
+     * @param array $params
+     * @param RequestHandler|null $requestHandler
+     * @return Response
+     * @throws Exception
+     */
+    public function post($path, array $params = [], RequestHandler $requestHandler = null)
+    {
+        $url = $this->getUrl($path, $params);
+        $callback = function($ch) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+        };
+        return $this->makeRequest($url, $callback, $requestHandler);
+    }
+
+    /**
+     * @param $path
+     * @param array $params
+     * @param RequestHandler|null $requestHandler
+     * @return Response
+     * @throws Exception
+     */
+    public function put($path, array $params = [], RequestHandler $requestHandler = null)
+    {
+        $url = $this->getUrl($path, $params);
+        $callback = function($ch) {
+            curl_setopt($ch, CURLOPT_PUT, 1);
+        };
+        return $this->makeRequest($url, $callback, $requestHandler);
+    }
 }
